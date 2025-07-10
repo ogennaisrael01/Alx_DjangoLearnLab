37d222fffa5211cbd25286124a576f12e59b7149
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-07-10 21:21:06 +0100

    app: add Book model in model.py file

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
-    publication_year = models.IntegerField(default=2025)
+    publication_year = models.IntegerField()
