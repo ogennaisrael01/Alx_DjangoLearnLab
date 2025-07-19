525fc5d6ce0e0c1d2a7f1735f21386a635675cc5
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-07-19 18:00:03 +0100

    djanog models: initial comiit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -7,10 +7,11 @@ def get_book_by_author(author_name):
     for book in books:
         print(book)
 
-def list_books(library_name):
+def list_books_in_library(library_name):
     books = Library.objects.get(name=library_name)
     books.all()
 
-def retrieve_liberian(name):
-    liberian = Librarian.objects.get(name=name).select_related('library')
-    print(liberian)
+def retrieve_liberian(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = Librarian.objects.get(library=library)
+    return librarian
