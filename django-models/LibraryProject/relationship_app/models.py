b987c60bd1acaa802ce20322b47c49692f41796f
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-07-19 17:31:39 +0100

    djanog models: initial comiit

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -5,14 +5,25 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+    
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="authors")
 
+    def __str__(self):
+        return self.title
+    
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
+    def __str__(self):
+        return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="library")
+    
+    def __str__(self):
+        return self.name
