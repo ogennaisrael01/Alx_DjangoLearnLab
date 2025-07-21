709871f1fa7f9b4d7d23c81c0a9992a20f8ebc3c
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-07-21 21:44:07 +0100

    djanog models: initial comiit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -37,20 +37,20 @@ def register(request):
 def is_admin(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
 
-@user_passes_test
+@user_passes_test(is_admin)
 def admin_view(request):
     return  render(request, "relationship_app/admin_view.html")
 
 def is_librarian(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Librarian"
 
-@user_passes_test
+@user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, "relationship_app/librarian_view.html")
 
 def is_member(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Member"
-@user_passes_test
+@user_passes_test(is_member)
 def member_view(request):
     return render(request, "relationship_app/member_view.html")
 
