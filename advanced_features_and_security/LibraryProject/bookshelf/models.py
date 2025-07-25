4da611da0e8366c6a606ac813fe8d7228cceff60
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-07-25 23:01:42 +0100

    add custom user model in relationship app

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import BaseUserManager
 
+class UserManager(BaseUserManager):
+    def create_user(self, email=None, password=None, **kwargs):
+        if not email:
+            return "Please provide your email address"
+        normalize_email = self.normalize_email(email)
+        user = self.model(email=normalize_email, password=password, **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password, **kwargs):
+        """ Method to create super user """
+
+        user = self.create_user(email=email, password=password, **kwargs)
+        user.is_admin = True
+        user.is_superuser = True
+
+        user.save(using=self._db)
+        return user
+        
+
+
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(blank=True, null=True, max_length=200)
+    profile_photo = models.ImageField(verbose_name="profile picture", blank=True, null=True)
 
 
 class Book(models.Model):
