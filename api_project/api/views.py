64ba2b3139c91ac7ef7ae0e1cf2e30195951e889
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-08-02 00:10:22 +0100

    feat: added the queryset method

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 # Create your views here.
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
 
     def create(self, request, *args, **kwargs):
@@ -19,6 +19,6 @@ class BookViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=400)
     
     def get_queryset(self):
-        queryset = self.get_queryset()
-        return queryset.objects.all()
+        """ Get a specific instance from the book model"""
+        return super().get_queryset()
 
