a88b27f877fd96ea65facc4fbd08984cea3c6dc1
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-08-01 22:38:44 +0100

    eat: created serializer for the blog model b2ba76f  · 4 minutes ago fear:  implemented views for list and create book model feat: add api endpoints to the create and lists views

--- api_project/api/serializers.py
+++ api_project/api/serializers.py
@@ -1,8 +1,8 @@
 from api.models import Book
-from rest_framework.serializers import ModelSerializer, DateTimeField
+from rest_framework import serializers
 import datetime
-class BookSerializer(ModelSerializer):
-    created_at = DateTimeField(default=datetime.datetime.now().strftime("%Y-%m-%d  %H,%M,%S"))
+class BookSerializer(serializers.ModelSerializer):
+    created_at = serializers.DateTimeField(default=datetime.datetime.now().strftime("%Y-%m-%d  %H,%M,%S"))
     class Meta:
         model = Book
         fields = ["title", "author", "created_at"]
