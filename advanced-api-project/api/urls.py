0f458ce42861f68b448225c1188388501e7e3dc0
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-08-14 17:07:18 +0100

    made some correction on the urls file

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from api.views import AuthorView, ListView, CreateView, DeleteView, UpdateView, DetialView
+from api.views import AuthorView, DetailView, ListView, CreateView, DeleteView, UpdateView
 
 urlpatterns = [
     """ API endpoints for the Book and Author resources """,
-    
+
     path("authors/", AuthorView.as_view(), name="author"),
     path('books/', ListView.as_view(), name="list-book"),
-    path('book/<int:pk>/', DetialView.as_view(), name="detail-view"),
-    path("book/create/", CreateView.as_view(), name="create-book"),
-    path('book/update/<int:pk>/', UpdateView.as_view(), name="update-book"),
-    path('book/delete/<int:pk>/', DeleteView.as_view(), name="delete-book")
+    path('books/<int:pk>/', DetailView.as_view(), name="detail-view"),
+    path("books/create/", CreateView.as_view(), name="create-book"),
+    path('books/update/<int:pk>/', UpdateView.as_view(), name="update-book"),
+    path('books/delete/<int:pk>/', DeleteView.as_view(), name="delete-book")
 ]
