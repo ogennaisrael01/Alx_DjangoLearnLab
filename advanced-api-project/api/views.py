6ed9d6484256e9c5024af777dbca72f8ccc6353a
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-08-14 17:24:09 +0100

    imported the permission  classes

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,19 +3,19 @@ from rest_framework import generics
 from api.models import Author, Book
 from api.serializers import AuthorSerializer, BookSerializer, BookInputSerializer
 from rest_framework.response import Response
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 class AuthorView(generics.ListAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class CreateView(generics.CreateAPIView):
     """ View to create a new Book instance. """
     # Using BookInputSerializer to handle input validation and creation
     queryset = Book.objects.all()
     serializer_class = BookInputSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def create(self, request, *args, **kwargs):
         """ Handle the creation of a new Book instance. """
@@ -29,23 +29,23 @@ class ListView(generics.ListAPIView):
     """ View to list all Book instances. """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly] # Allows unauthenticated users to view the list 
+    permission_classes = [IsAuthenticatedOrReadOnly] # Allows unauthenticated users to view the list 
 
 class DetailView(generics.RetrieveAPIView):
     """ View to retrieve a single Book instance. """
     queryset = Book.objects.all().order_by("title")
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class UpdateView(generics.UpdateAPIView):
     """ View to update an existing Book instance. """
     # Using BookInputSerializer to handle input validation and update
     queryset = Book.objects.all()
     serializer_class = BookInputSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 class DeleteView(generics.DestroyAPIView):
     """ View to delete a Book instance. """
     queryset = Book.objects.all()
     serializer_class = BookInputSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes =[IsAuthenticated]
