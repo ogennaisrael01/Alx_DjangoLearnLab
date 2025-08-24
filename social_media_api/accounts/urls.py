e6040b912d9eadce5fb38685f8e36672189f45fa
ogennaisrael01 <ogennaisrael98@gmail.com>
2025-08-24 10:50:57 +0100

    configured urls for follow and unfollow using DEFAULTROUTHER

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, include
 from accounts import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'users', views.UserApiView)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path("register/", views.RegistrationApiView.as_view(), name="register"),
     path("login/", views.LoginApiView.as_view(), name="login"),
 ]
